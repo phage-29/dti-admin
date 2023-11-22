0662d7879881f9fe51bac1da5e2ab8cd8b721d96
Phage <dace.phage@gmail.com>
2023-11-22 13:53:11 +0800

    graph

--- dashboard.php
+++ dashboard.php
@@ -118,43 +118,65 @@ require_once "components/sidebar.php";
                     data: [
                       <?php
                       $query = "SELECT
-                                    d.`Division`,
-                                    COUNT(h.`DivisionID`) AS `count_per_division`
-                                FROM `helpdesks` h
-                                    LEFT JOIN `divisions` d ON h.`DivisionID` = d.`id`
-                                GROUP BY d.`Division`
-                                ORDER BY d.`Division`";
+                  d.`Division`,
+      COUNT(h.`DivisionID`) AS`count_per_division`
+              FROM`helpdesks` h
+                  LEFT JOIN`divisions` d ON h.`DivisionID` = d.`id`
+              GROUP BY d.`Division`
+              ORDER BY d.`Division`";
                       $result = $conn->execute_query($query);
                       while ($row = $result->fetch_object()) {
                       ?> <?= $row->count_per_division ?>,
                       <?php
                       }
                       ?>
-                    ]
+                    ],
                   }],
                   chart: {
                     type: 'bar',
-                    height: 350
+                    height: 380
                   },
                   plotOptions: {
                     bar: {
-                      borderRadius: 4,
+                      barHeight: '100%',
+                      distributed: true,
                       horizontal: true,
+                      dataLabels: {
+                        position: 'bottom'
+                      },
                     }
                   },
+                  // colors: ['#33b2df', '#546E7A', '#d4526e', '#13d8aa', '#A5978B', '#2b908f', '#f9a3a4', '#90ee7e',
+                  //   '#f48024', '#69d2e7'
+                  // ],
                   dataLabels: {
-                    enabled: false
+                    enabled: true,
+                    textAnchor: 'start',
+                    style: {
+                      colors: ['#fff']
+                    },
+                    formatter: function(val, opt) {
+                      return opt.w.globals.labels[opt.dataPointIndex] + ":  " + val
+                    },
+                    offsetX: 0,
+                    dropShadow: {
+                      enabled: true
+                    }
+                  },
+                  stroke: {
+                    width: 1,
+                    colors: ['#fff']
                   },
                   xaxis: {
                     categories: [
                       <?php
                       $query = "SELECT
-                                    d.`Division`,
-                                    COUNT(h.`DivisionID`) AS `count_per_division`
-                                FROM `helpdesks` h
-                                    LEFT JOIN `divisions` d ON h.`DivisionID` = d.`id`
-                                GROUP BY d.`Division`
-                                ORDER BY d.`Division`";
+                  d.`Division`,
+    COUNT(h.`DivisionID`) AS`count_per_division`
+              FROM`helpdesks` h
+                  LEFT JOIN`divisions` d ON h.`DivisionID` = d.`id`
+              GROUP BY d.`Division`
+              ORDER BY d.`Division`";
                       $result = $conn->execute_query($query);
                       while ($row = $result->fetch_object()) {
                       ?> '<?= $row->Division ?>',
@@ -162,6 +184,25 @@ require_once "components/sidebar.php";
                       }
                       ?>
                     ],
+                  },
+                  yaxis: {
+                    labels: {
+                      show: false
+                    }
+                  },
+                  
+                  tooltip: {
+                    theme: 'dark',
+                    x: {
+                      show: false
+                    },
+                    y: {
+                      title: {
+                        formatter: function() {
+                          return ''
+                        }
+                      }
+                    }
                   }
                 }).render();
               });
