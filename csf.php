c408f191b8ac4801087864c723568b14269cbc57
Phage <dace.phage@gmail.com>
2023-11-24 15:21:27 +0800

    update

--- csf.php
+++ csf.php
@@ -3,7 +3,13 @@ require_once "includes/conn.php";
 
 if (isset($_GET['RequestNo'])) {
     $query = "UPDATE `helpdesks` SET `Csf` = ? WHERE `RequestNo` = ?";
-    $result = $conn->execute_query($query,['Done', $_GET['RequestNo']]);
+    $result = $conn->execute_query($query, ['Done', $_GET['RequestNo']]);
 
-    header('Location: https://forms.office.com/r/tBGKen7rG6');
+?>
+    <script>
+        setTimeout(function() {
+            window.location.href = "https://forms.office.com/r/tBGKen7rG6";
+        }, 1000)
+    </script>
+<?php
 }
